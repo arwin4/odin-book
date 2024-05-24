36f9a64209c7be0a06e5e762d4cb89d116a318c2
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-05-24 14:02:46 +0200

    Use ImageUploadBox in NewPost

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -1,8 +1,9 @@
 import getJwt from '@utils/getJwt';
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import { Form, redirect, useNavigation } from 'react-router-dom';
 
 import './style/NewPost.css';
+import ImageUploadBox from '@components/ImageUploadBox';
 
 /**
  * TODO:
@@ -15,85 +16,9 @@ export default function NewPostForm() {
   const navigation = useNavigation();
   const uploading = navigation.state !== 'idle';
 
-  const [file, setFile] = useState(null);
-  const inputRef = useRef();
-  const dropBoxRef = useRef();
-
-  const handleDragEnter = (e) => {
-    dropBoxRef.current.classList.add('dragging');
-    e.stopPropagation();
-    e.preventDefault();
-  };
-
-  const handleDragOver = (e) => {
-    e.stopPropagation();
-    e.preventDefault();
-  };
-
-  const handleDragLeave = (e) => {
-    e.stopPropagation();
-    dropBoxRef.current.classList.remove('dragging');
-    e.preventDefault();
-  };
-
-  const handleDrop = (e) => {
-    e.stopPropagation();
-    dropBoxRef.current.classList.remove('dragging');
-    e.preventDefault();
-
-    const { dataTransfer } = e;
-    const dropFile = dataTransfer.files[0];
-    setFile(dropFile);
-
-    // Add the image to the input's FileList
-    const newTransfer = new DataTransfer();
-    newTransfer.items.add(dropFile);
-    inputRef.current.files = newTransfer.files;
-  };
-
-  const isFileValid = (curFile) => {
-    if (!curFile) return false;
-    if (!curFile.type.startsWith('image/')) return false;
-    return curFile.size <= 10 ** 7;
-  };
-
   return (
     <Form className="new-post-form" method="post" encType="multipart/form-data">
-      <label
-        htmlFor="file"
-        className="dropbox"
-        onDragEnter={handleDragEnter}
-        onDragOver={handleDragOver}
-        onDrop={handleDrop}
-        onDragLeave={handleDragLeave}
-        onChange={(e) => setFile(e.target.files[0])}
-        ref={dropBoxRef}
-      >
-        <div className="content-wrapper">
-          {!isFileValid(file) && (
-            <div className="instruction">
-              Drop an image here or click to upload
-            </div>
-          )}
-
-          {isFileValid(file) && <img src={URL.createObjectURL(file)} alt="" />}
-
-          {inputRef?.current?.files.length > 0 && !isFileValid(file) && (
-            <div className="filesize-warning">
-              File is too big. Please select an image smaller than 10 MB.
-            </div>
-          )}
-
-          <input
-            type="file"
-            name="file"
-            id="file"
-            accept="image/*"
-            ref={inputRef}
-            required
-          />
-        </div>
-      </label>
+      <ImageUploadBox />
       <input
         type="text"
         placeholder="Add a description..."
