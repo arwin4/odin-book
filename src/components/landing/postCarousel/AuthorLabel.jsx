c8c1b63b5cea874863ecfe4c38760edcfa7b61e6
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-05-28 13:32:54 +0200

    Make author label clickable

--- src/components/landing/postCarousel/AuthorLabel.jsx
+++ src/components/landing/postCarousel/AuthorLabel.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import postPropType from '@propTypes/post';
 import { InlineIcon } from '@iconify/react';
+import { Link } from 'react-router-dom';
 
 export default function AuthorLabel({ post }) {
   const { firstName, username, avatarUrl } =
     post.relationships.author.data.attributes;
 
   return (
-    <div className="author-label">
+    <Link className="author-label" to={`/user/${username}`}>
       <img src={avatarUrl} className="avatar" alt="" />
       <div className="text-label">
         <InlineIcon className="icon" icon="ph:user-circle" />
         <span className="first-name">{firstName}</span>
         <span className="username">@{username}</span>
       </div>
-    </div>
+    </Link>
   );
 }
 
