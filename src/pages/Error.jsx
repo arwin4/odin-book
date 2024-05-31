dce3a4393207c4c852266b25c4533f6208de5c73
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-05-31 15:01:30 +0200

    Add Navbar to Error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,25 +1,20 @@
 import Navigation from '@components/navigation/Navigation';
 import React from 'react';
-import { Link, Navigate, useNavigation, useRouteError } from 'react-router-dom';
+import { Navigate, useRouteError } from 'react-router-dom';
 import './style/Error.css';
 
 export default function Error() {
   const error = useRouteError();
   console.log(error);
-  // console.log(error);
-  // console.log(error.status);
-  // const navigation = useNavigation();
 
   switch (error.status) {
-    // case 400:
-    //   return <div className="error">{/* {error.data} {backToChatsBtn} */}</div>;
-
     case 404:
       return (
         <>
           <Navigation />
           <div className="error">
-            <h1>404: Not Found</h1>
+            <h1>404</h1>
+            <h2>{error.data}</h2>
           </div>
         </>
       );
@@ -28,10 +23,13 @@ export default function Error() {
       return <Navigate to="/login" />;
     default:
       return (
-        <div className="error">
-          An error occurred. {error.message}
-          {/* {toLoginBtn} */}
-        </div>
+        <>
+          <Navigation />
+          <div className="error">
+            <h1>An error occurred.</h1>
+            <h2>{error.message}</h2>
+          </div>
+        </>
       );
   }
 }
