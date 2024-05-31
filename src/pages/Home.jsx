fff8e74b5feb17260ef523d99c33e4a701067533
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-05-31 15:00:24 +0200

    Handle errors in homeLoader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,8 +38,8 @@ export async function homeLoader() {
   );
 
   if (!res.ok) {
-    // const { errors } = await res.json();
-    // throw new Response(errors[0].title, { status: res.status });
+    const { errors } = await res.json();
+    throw new Response(errors[0].title, { status: res.status });
   }
   const parsedFetchedPosts = await res.json();
   return parsedFetchedPosts.data;
