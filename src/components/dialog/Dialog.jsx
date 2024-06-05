56eb6bd7474ac5b6377ce947e168c23913c8c612
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-06-05 15:39:01 +0200

    Add light dismiss to dialog

--- src/components/dialog/Dialog.jsx
+++ src/components/dialog/Dialog.jsx
@@ -3,20 +3,33 @@ import PropTypes from 'prop-types';
 import { InlineIcon } from '@iconify/react';
 import CloseButton from './CloseButton';
 
-// TODO: add soft dismiss
+const Dialog = forwardRef(({ children, title, icon }, ref) => {
+  // Enable light dismiss. Source: https://stackoverflow.com/a/76466616/22857578
+  if (ref.current) {
+    ref.current.addEventListener('click', ({ target: dialog }) => {
+      if (dialog.nodeName === 'DIALOG') {
+        // NOTE: Disabling the transition temporarily is necessary to prevent
+        // the position of the dialog from shifting during closing.
+        dialog.classList.add('disable-transition');
+        dialog.close('dismiss');
+        dialog.classList.remove('disable-transition');
+      }
+    });
+  }
 
-const Dialog = forwardRef(({ children, title, icon }, ref) => (
-  <dialog ref={ref}>
-    <header>
-      <InlineIcon className="icon" icon={icon} height="unset" />
-      <h1>{title}</h1>
-      <form className="close-btn" method="dialog">
-        <CloseButton icon="ph:x-circle" type="submit" />
-      </form>
-    </header>
-    {children}
-  </dialog>
-));
+  return (
+    <dialog ref={ref}>
+      <header>
+        <InlineIcon className="icon" icon={icon} height="unset" />
+        <h1>{title}</h1>
+        <form className="close-btn" method="dialog">
+          <CloseButton icon="ph:x-circle" type="submit" />
+        </form>
+      </header>
+      {children}
+    </dialog>
+  );
+});
 
 export default Dialog;
 
