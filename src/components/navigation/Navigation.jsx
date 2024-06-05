1fd3407ee0a622d94f4e15e4e345d848f0cbb481
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-06-05 17:11:56 +0200

    Hide NavBar on scroll down, show on scroll up

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import getCurrentUser from '@utils/getCurrentUser';
@@ -7,9 +7,23 @@ import './style/Navigation.css';
 
 export default function Navigation() {
   const currentUser = getCurrentUser();
+  const navBarRef = useRef();
+
+  useEffect(() => {
+    let lastScrollPosition = window.scrollY;
+    window.addEventListener('scroll', () => {
+      // NOTE: Also fires when window is resized
+      if (window.scrollY > lastScrollPosition) {
+        navBarRef.current.classList.remove('show-on-scroll-up');
+      } else if (window.scrollY < lastScrollPosition) {
+        navBarRef.current.classList.add('show-on-scroll-up');
+      }
+      lastScrollPosition = window.scrollY;
+    });
+  }, [window.scrollY]);
 
   return (
-    <nav className="nav-bar">
+    <nav className="nav-bar" ref={navBarRef}>
       <NavLink className="item" to="/">
         <div className="description">My feed</div>
       </NavLink>
