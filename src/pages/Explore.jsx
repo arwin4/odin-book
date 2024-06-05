300c27fa1cd2c02a1369162d4b3d9a88b9d047f3
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-06-05 12:24:41 +0200

    Add infinite scroll

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Posts from '@components/home/Posts';
-import { useLoaderData, useRevalidator } from 'react-router-dom';
+import { Link, useLoaderData, useRevalidator } from 'react-router-dom';
+import { InlineIcon } from '@iconify/react';
 
 export default function Explore() {
   const posts = useLoaderData();
   console.log(posts);
   const revalidator = useRevalidator();
+  const intersectionRef = useRef();
 
   function fetchMore() {
     // FIXME: use navigate instead. When using React Router, the history API
@@ -20,12 +22,56 @@ export default function Explore() {
     revalidator.revalidate();
   }
 
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        fetchMore();
+      }
+    });
+
+    if (intersectionRef.current) {
+      observer.observe(intersectionRef.current);
+    }
+
+    return () => {
+      if (intersectionRef.current) {
+        observer.unobserve(intersectionRef.current);
+      }
+    };
+  }, [intersectionRef, posts.length]);
+
+  if (posts.length === 0) {
+    return (
+      <div className="home">
+        <div className="no-posts-tip">
+          <h2>No posts yet!</h2>
+          <h3>
+            Head over to{' '}
+            <Link className="explore-link" to="/explore">
+              <em>Explore</em>
+            </Link>{' '}
+            to find some users to follow.
+          </h3>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <>
+    <div className="timeline">
       <Posts posts={posts} />
-      <button onClick={fetchMore} type="button">
-        More
-      </button>
-    </>
+
+      <div className="intersection" ref={intersectionRef}>
+        {revalidator.state === 'loading' ? (
+          <InlineIcon
+            className="icon"
+            icon="svg-spinners:ring-resize"
+            height="unset"
+          />
+        ) : (
+          'No more posts to show.'
+        )}
+      </div>
+    </div>
   );
 }
