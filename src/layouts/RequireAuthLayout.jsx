9b29fdb5cd62c9512a5dae55784cc78f59e26e49
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-06-06 14:10:06 +0200

    Add pending UI

--- src/layouts/RequireAuthLayout.jsx
+++ src/layouts/RequireAuthLayout.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import RequireAuth from '@components/auth/RequireAuth';
 import NavBar from '@components/navbar/NavBar';
 
 export default function RequireAuthLayout() {
+  const navigation = useNavigation();
+
   return (
     <RequireAuth>
       <NavBar />
-      <main>
+      <main className={navigation.state === 'loading' ? 'loading' : ''}>
         <Outlet />
       </main>
     </RequireAuth>
