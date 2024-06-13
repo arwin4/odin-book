749dc0b49f3f46cdd6db396e5b392f37d1298fcc
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-06-13 12:10:27 +0200

    Add access options to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import useAuth from '@hooks/useAuth';
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
 import PostCarousel from '@components/landing/postCarousel/PostCarousel';
 
 import './style/Landing.css';
 import LabelButton from '@components/buttons/LabelButton';
+import { InlineIcon } from '@iconify/react';
 
 export default function Landing() {
-  const { authed } = useAuth();
-  const { state } = useLocation();
+  // const { authed } = useAuth();
+  // const { state } = useLocation();
 
-  if (authed) {
-    return <Navigate to={state?.path || '/'} />;
-  }
+  // if (authed) {
+  //   return <Navigate to={state?.path || '/'} />;
+  // }
+
+  const landingRef = useRef();
+  const navigate = useNavigate();
+
+  const enter = () => {
+    landingRef.current.classList.add('fade-out');
+    setTimeout(() => {
+      navigate('/login');
+    }, 250);
+  };
 
   return (
-    <div className="landing-container">
+    <div className="landing-container" ref={landingRef}>
       <h1 className="title">Odinstagram</h1>
 
       <PostCarousel />
 
       <div className="call-to-action">
-        <LabelButton text="Enter »" />
+        <LabelButton text="Sign up »" onClick={enter} />
+        <LabelButton text="Log in »" onClick={enter} />
+        <div className="guest-account-tip">
+          <div className="text">Guest account available</div>
+          <InlineIcon
+            className="icon"
+            icon="ph:arrow-bend-right-up"
+            height="unset"
+          />
+        </div>
       </div>
     </div>
   );
