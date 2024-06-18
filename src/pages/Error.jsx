31910e8ebed7ce0eda3c53a26526ab390a8372ce
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-06-18 14:13:03 +0200

    Add explanation to 404

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -15,7 +15,11 @@ export default function Error() {
           <main>
             <div className="error">
               <h1>404</h1>
-              <h2>{error.data}</h2>
+              <h2>This page does not exist.</h2>
+              <h3>
+                If you were expecting a user or a post here, the user or post
+                may no longer exist.
+              </h3>
             </div>
           </main>
         </>
@@ -25,12 +29,12 @@ export default function Error() {
       return <Navigate to="/welcome" />;
     default:
       return (
-          <main>
-            <div className="error">
-              <h1>An error occurred.</h1>
-              <h2>{error.message}</h2>
-            </div>
-          </main>
+        <main>
+          <div className="error">
+            <h1>An error occurred.</h1>
+            <h2>{error.message}</h2>
+          </div>
+        </main>
       );
   }
 }
